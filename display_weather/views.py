e6ab00c57ace7d17c93baaf6416c7c9cd0d28f97
Efaz Khan <efazahmed@hotmail.com>
2019-09-14 22:09:50 -0400

    Added unsplash api to display weather related images

--- display_weather/views.py
+++ display_weather/views.py
@@ -3,6 +3,7 @@ import os
 from django.shortcuts import render
 
 api_key = os.environ.get('OPEN_WEATHER_API')
+unsplash_api_key = os.environ.get('UNSPLASH_API')
 
 
 # Create your views here.
@@ -17,8 +18,13 @@ def display(request):
 		temp_min = response.json()['main']['temp_min']
 		humidity = response.json()['main']['humidity']
 		weather_description = response.json()['weather'][0]['description']
+
+		response_img = requests.get(f"https://api.unsplash.com/search/photos?query={weather_description}&client_id={unsplash_api_key}")
+		img_url = response_img.json()['results'][0]['urls']['small']
+
 		return render(request, "display_weather/display.html", {'city':city, 'temp':temp, 'temp_max':temp_max, 'temp_min':temp_min, 
-																'weather_description':weather_description, 'humidity':humidity})
+																'weather_description':weather_description, 'humidity':humidity,
+																'img_url':img_url})
 
 	else:	
 		return render(request, "display_weather/display.html")
